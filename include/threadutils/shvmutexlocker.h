1be4e55b3a11f2177af3cc480dadc2643bb16f8d
mbn <mbn>
2008-02-12 15:17:53 +0000

    Added SHVMutexLocker convience class

---
+++ include/threadutils/shvmutexlocker.h
@@ -0,0 +1,31 @@
+#ifndef __SHIVA_THREADUTILS_MUTEXLOCKER_H
+#define __SHIVA_THREADUTILS_MUTEXLOCKER_H
+
+#include "shvmutex.h"
+
+//-=========================================================================================================
+///  SHVMutexLocker class - convenience class to handle function locks
+/**
+ * Convenience class to handle function locks. See example.
+\code
+	void examplefunc()
+	{
+	SHVMutexLock lock(myLock);
+		doSomething();
+		if (checkSomeThing())
+			return; // While result in "lock" being destroyed and mutex unlocked.
+		doSomethingElse();
+	}
+\endcode
+ */
+
+class SHVMutexLocker
+{
+public:
+	SHVMutexLocker(SHVMutex* Mutex) { Mutex->Lock();   }
+	~SHVMutexLocker()               { Mutex->Unlock(); }
+private:
+	SHVMutex* Mutex;
+};
+
+#endif
