228752389addf26e9d160895a49cf0c47b4d3742
ler <ler>
2008-02-26 09:37:10 +0000

    added conversion methods between pixels and logical font units. Logical font units (short : LFU) are 1/10ths of font width and height.

--- include/gui/utils/shvfont.h
+++ include/gui/utils/shvfont.h
@@ -37,9 +37,44 @@ public:
 	virtual SHVStringBuffer GetName() = 0;
 
 
+	// Conversion between font units and pixels
+	inline int LFUToWidth(int width);
+	inline int LFUToHeight(int height);
+	inline int WidthToLFU(int width);
+	inline int HeightToLFU(int height);
+
+
 protected:
 	inline SHVFont() : SHVRefObject() {}
 };
 typedef SHVRefObjectContainer<SHVFont> SHVFontRef;
+
+
+
+// ============================================= implementation ============================================= //
+
+
+/*************************************
+ * LFUToWidth
+ *************************************/
+int SHVFont::LFUToWidth(int width)
+{
+	return ((width*10+5)*GetApproximateWidth())/100;
+}
+
+int SHVFont::LFUToHeight(int height)
+{
+	return ((height*10+5)*GetCellHeight())/100;
+}
+
+int SHVFont::WidthToLFU(int width)
+{
+	return (((100*width)+5)/GetApproximateWidth())/10;
+}
+
+int SHVFont::HeightToLFU(int height)
+{
+	return (((100*height)+5)/GetCellHeight())/10;
+}
 
 #endif
