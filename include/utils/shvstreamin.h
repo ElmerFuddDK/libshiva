672d3e115a3d9744192255350ed9fc618c69376e
mbn <mbn>
2008-03-11 12:40:09 +0000

    Added stream interface

---
+++ include/utils/shvstreamin.h
@@ -0,0 +1,53 @@
+#ifndef __SHIVA_UTILS_STREAMIN_H
+#define __SHIVA_UTILS_STREAMIN_H
+
+#include "../shvtypes.h"
+#include "../shvstring.h"
+//-=========================================================================================================
+/// SHVStreamIn class - Stream input interface
+/**
+ */
+class SHVStreamIn
+{
+public:
+	virtual ~SHVStreamIn() {}
+	virtual bool Eof() const = 0;
+	virtual SHVBool IsOk() const =  0;
+	virtual SHVBool ReadBuffer(void* buffer, int bufferSize, int& actualLength) = 0;
+
+	virtual SHVBool ReadString16(SHVWChar* buffer, int maxlen) = 0;
+	virtual SHVWChar ReadChar16() = 0;
+
+	virtual SHVBool ReadString8(SHVChar* buffer, int maxlen) = 0;
+	virtual SHVChar ReadChar8() = 0;
+
+	inline SHVBool ReadString(SHVTChar* buffer, int maxlen);
+	inline SHVTChar ReadChar();
+};
+
+// ============================================= implementation ============================================= //
+/*************************************
+ * ReadString
+ *************************************/
+SHVBool SHVStreamIn::ReadString(SHVTChar* buffer, int maxlen)
+{
+#ifdef UNICODE
+	return ReadString16(buffer, maxlen);
+#else
+	return ReadString8(buffer, maxlen);
+#endif
+}
+
+/*************************************
+ * ReadChar
+ *************************************/
+SHVTChar SHVStreamIn::ReadChar()
+{
+#ifdef UNICODE
+	return ReadChar16();
+#else
+	return ReadChar8();
+#endif
+}
+
+#endif
---
+++ include/utils/shvstreamout.h
@@ -0,0 +1,52 @@
+#ifndef __SHIVA_UTILS_STREAMIN_H
+#define __SHIVA_UTILS_STREAMIN_H
+
+#include "../shvtypes.h"
+#include "../shvstring.h"
+//-=========================================================================================================
+/// SHVStreamOut class - Stream out interface
+/**
+ */
+class SHVStreamOut
+{
+public:
+	virtual ~SHVStreamOut() {}
+	virtual SHVBool IsOk() const =  0;
+	virtual SHVBool WriteBuffer(void* buffer, int len) = 0;
+
+	virtual SHVBool WriteString16(SHVWChar* buffer, int maxlen = -1) = 0;
+	virtual SHVBool WriteChar16(SHVWChar ch) = 0;
+
+	virtual SHVBool WriteString8(SHVChar* buffer, int maxlen = -1) = 0;
+	virtual SHVBool WriteChar8(SHVChar ch) = 0;
+
+	inline SHVBool WriteString(SHVTChar* buffer, int maxlen = -1);
+	inline SHVBool WriteChar(SHVTChar);
+};
+
+// ============================================= implementation ============================================= //
+/*************************************
+ * ReadString
+ *************************************/
+SHVBool SHVStreamOut::WriteString(SHVTChar* buffer, int maxlen)
+{
+#ifdef UNICODE
+	return WriteString16(buffer, maxlen);
+#else
+	return WriteString8(buffer, maxlen);
+#endif
+}
+
+/*************************************
+ * ReadChar
+ *************************************/
+SHVBool SHVStreamOut::WriteChar(SHVTChar ch)
+{
+#ifdef UNICODE
+	return WriteChar16(ch);
+#else
+	return WriteChar8(ch);
+#endif
+}
+
+#endif
