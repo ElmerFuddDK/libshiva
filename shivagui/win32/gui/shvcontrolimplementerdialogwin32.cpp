360d7a87327a2e863bba3d841e998bd6cf9d2b6a
ler <ler>
2008-06-24 17:30:22 +0000

    Added the license to the source files.

--- shivagui/win32/gui/shvcontrolimplementerdialogwin32.cpp
+++ shivagui/win32/gui/shvcontrolimplementerdialogwin32.cpp
@@ -3,7 +3,7 @@
  *
  *
  *   This program is free software; you can redistribute it and/or modify
- *1it under the terms of the GNU Library General Public License as
+ *   it under the terms of the GNU Library General Public License as
  *   published by the Free Software Foundation; either version 2 of the
  *   License, or (at your option) any later version with the following
  *   exeptions:
