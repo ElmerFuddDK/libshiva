e0d5ad5bc732fcce7f72d5f7a08c6d937d528501
ler <ler>
2009-07-31 21:57:32 +0000

    Bugfix: SHVPoint didn't work as a value for ... operators with gcc, hence was unusable for SHVDraw. Fixed this by adding a C style struct that can hold the value of a SHVPoint.

--- include/gui/utils/shvpoint.h
+++ include/gui/utils/shvpoint.h
@@ -3,6 +3,16 @@
 
 
 //-=========================================================================================================
+/// SHVPointVal - struct for holding a C style point 
+/**
+ * gcc doesn't like non C type data in ... operators so for portable
+ * code use this in SHVDraw ... operators
+ */
+
+struct SHVPointVal { int x,y; };
+
+
+//-=========================================================================================================
 /// SHVPoint - simple class with x,y coords
 /**
  */
@@ -11,12 +21,15 @@ class SHVPoint
 {
 public:
 
-
+	inline SHVPoint(const SHVPointVal& val);
 	inline SHVPoint(int _x = 0, int _y = 0);
 	
 	inline SHVPoint& Move(int _x,int _y);
 
 	int x, y;
+
+	inline SHVPointVal Val();
+	inline static SHVPointVal Val(int x, int y);
 
 };
 
@@ -27,18 +40,37 @@ public:
 /*************************************
  * Constructor
  *************************************/
+SHVPoint::SHVPoint(const SHVPointVal& val) : x(val.x), y(val.y)
+{}
 SHVPoint::SHVPoint(int _x, int _y) : x(_x), y(_y)
-{
-}
+{}
 
 /*************************************
- * Constructor
+ * Move
  *************************************/
 SHVPoint& SHVPoint::Move(int _x,int _y)
 {
 	x += _x;
 	y += _y;
 	return *this;
+}
+
+/*************************************
+ * ToVal
+ *************************************/
+SHVPointVal SHVPoint::Val()
+{
+SHVPointVal r={x,y};
+	return r;
+}
+
+/*************************************
+ * Val
+ *************************************/
+SHVPointVal SHVPoint::Val(int x, int y)
+{
+SHVPointVal r={x,y};
+	return r;
 }
 
 #endif
