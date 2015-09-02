b9f3b224859056f94fac8c2fb769e7e15174399b
Lars Eriksen <le@elmerfudd.gazolini.lan>
2015-09-02 21:16:05 +0200

    iOS: Added iOS define
    Added a SHIVA define for iOS, and one for the iOS
    simulator.

--- shiva/include/platforms/osx/platformspc.h
+++ shiva/include/platforms/osx/platformspc.h
@@ -1,1 +1,9 @@
 #include "../posix/platformspc.h"
+#include <TargetConditionals.h>
+
+#if TARGET_OS_EMBEDDED == 1
+# define __SHIVA_POSIX_IPHONEOS
+#elif TARGET_IPHONE_SIMULATOR == 1
+# define __SHIVA_POSIX_IPHONEOS
+# define __SHIVA_POSIX_IPHONEOS_SIMULATOR
+#endif
